58a63e83cf7b1d181f72ed666fddd3fb919c1d67
naryd.rakotonirina <naryd.rakotonirina@gmail.com>
2026-05-19 17:18:32 +0200

    Update main.py
    virgule sur CORS

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ app.add_middleware(
     ],
     
  # ✅ accepte toutes les URLs Vercel (preview inclus)
-    allow_origin_regex=r"https://.*\.vercel\.app"
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
